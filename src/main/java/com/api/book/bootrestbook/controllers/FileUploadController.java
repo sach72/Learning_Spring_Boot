cd28288ce71715a8cd4da57ffb213646a8248f7e
Sachin <bhattsachin993@gmail.com>
2026-01-20 23:28:57 +0530

    Added File upload API

---
+++ src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
@@ -0,0 +1,47 @@
+package com.api.book.bootrestbook.controllers;
+
+import java.net.http.HttpRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.api.book.bootrestbook.helper.FileUploadHelper;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadHelper fileuploadhelper;
+
+    @PostMapping("/upload-file")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        // System.out.println(file.getOriginalFilename());
+        // System.out.println(file.getResource());
+        // System.out.println(file.getClass());
+        // System.out.println(file.getContentType());
+
+        try {
+
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request Must Contain File!!");
+            }
+            if (!file.getContentType().equals("image/jpeg")) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only jpeg format is allowed!!");
+            }
+            boolean f = fileuploadhelper.uploadFile(file);
+            if (f) {
+                return ResponseEntity.ok("File uploaded successful!!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong wile uploading file!");
+    }
+}
---
+++ src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
@@ -0,0 +1,47 @@
+package com.api.book.bootrestbook.helper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+
+    public final String UPLOAD_DIR = "E:\\Eclipse_Project_Workspace\\bootrestbook\\src\\main\\resources\\static\\images";
+
+    public boolean uploadFile(MultipartFile multipartfile) {
+        boolean f = false;
+
+        try {
+
+            InputStream is = multipartfile.getInputStream();
+            byte data[] = new byte[is.available()];
+            is.read(data);
+
+            // write
+            FileOutputStream fos = new FileOutputStream(
+                    UPLOAD_DIR + File.separator + multipartfile.getOriginalFilename());
+            fos.write(data);
+            fos.flush();
+            fos.close();
+
+            Files.copy(multipartfile.getInputStream(),
+                    Paths.get(UPLOAD_DIR + File.separator + multipartfile.getOriginalFilename()),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            f = true;
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+}
