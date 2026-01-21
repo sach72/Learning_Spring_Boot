cb179e724e750837ff220af01e27cda24f9d2d41
Sachin <bhattsachin993@gmail.com>
2026-01-21 22:33:07 +0530

    Added dynamic url for uploading files

--- src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
@@ -1,7 +1,5 @@
 package com.api.book.bootrestbook.controllers;
 
-import java.net.http.HttpRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.api.book.bootrestbook.helper.FileUploadHelper;
 
@@ -35,7 +34,9 @@ public class FileUploadController {
             }
             boolean f = fileuploadhelper.uploadFile(file);
             if (f) {
-                return ResponseEntity.ok("File uploaded successful!!");
+                // return ResponseEntity.ok("File uploaded successful!!");
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/images/")
+                        .path(file.getOriginalFilename()).toUriString());
             }
 
         } catch (Exception e) {
